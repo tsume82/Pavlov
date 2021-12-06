77ce871f936cc88e4e49a9723f74159d5a5a1321
texflip <michele.tessari12@gmail.com>
2021-12-06 17:38:55 +0000

    ecperiments

---
+++ train2.bat
@@ -0,0 +1,2 @@
+python main.py train -d "./experiments/De ppo normal_sampled" -cd "/RosenbrockRotated_5D" -c exp22
+python main.py train -d "./experiments/De ppo normal_sampled" -cd "/RosenbrockRotated_10D" -c exp23
---
+++ train3.bat
@@ -0,0 +1,3 @@
+python main.py train -d "./experiments/De ppo normal_sampled" -cd "/SchaffersIllConditioned_5D" -c exp25
+python main.py train -d "./experiments/De ppo normal_sampled" -cd "/SchaffersIllConditioned_10D" -c exp26
+python main.py train -d "./experiments/De ppo normal_sampled" -cd "/SchaffersIllConditioned_20D" -c exp27
